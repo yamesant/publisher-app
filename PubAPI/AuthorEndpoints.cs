153d51dfa447077f94f2d3f47040d38698554247
yamesant <165981998+yamesant@users.noreply.github.com>
2024-10-13 22:23:19 +1100

    All author endpoints to use DTOs

--- PubAPI/AuthorEndpoints.cs
+++ PubAPI/AuthorEndpoints.cs
@@ -10,61 +10,63 @@ public static class AuthorEndpoints
     {
         var group = routes.MapGroup("/api/Author").WithTags(nameof(Author));
 
-        group.MapGet("/WithBooks/", async (PubContext db) =>
-        {
-            return await db.Authors.Include(a => a.Books).AsNoTracking().ToListAsync();
-        })
-        .WithName("GetAllAuthorsWithBooks")
-        .WithOpenApi();
-
-        group.MapGet("/{authorid}", async Task<Results<Ok<AuthorDTO>, NotFound>> (int authorid, PubContext db) =>
-        {
-            return await db.Authors.AsNoTracking()
-                .Where(model => model.AuthorId == authorid)
-                .Select(a => new AuthorDTO(a.AuthorId, a.Name.FirstName, a.Name.LastName))
-                .FirstOrDefaultAsync()
-                is AuthorDTO model
-                    ? TypedResults.Ok(model)
-                    : TypedResults.NotFound();
-        })
-        .WithName("GetAuthorById")
-        .WithOpenApi();
-
-
-        group.MapPut("/{authorid}", async Task<Results<Ok, NotFound>> (int authorid, AuthorDTO authorDTO, PubContext db) =>
-      {
-          var affected = await db.Authors
-              .Where(model => model.AuthorId == authorid)
-              .ExecuteUpdateAsync(setters => setters
-                  //.SetProperty(m => m.AuthorId, authorDTO.AuthorId)
-                  .SetProperty(m => m.Name.FirstName, authorDTO.FirstName)
-                  .SetProperty(m => m.Name.LastName, authorDTO.LastName)
-                  );
-          return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
-      })
-      .WithName("UpdateAuthor")
-      .WithOpenApi();
-
-        group.MapPost("/", async (AuthorDTO authorDTO, PubContext db) =>
-        {
-            var author = new Author { Name = new PersonName { FirstName = authorDTO.FirstName, LastName = authorDTO.LastName }};
-            db.Authors.Add(author);
-            await db.SaveChangesAsync();
-            return TypedResults.Created($"/api/Author/{author.AuthorId}",
-                new AuthorDTO(author.AuthorId, author.Name.FirstName, author.Name.LastName));
-        })
-        .WithName("CreateAuthor")
-        .WithOpenApi();
-
-        group.MapDelete("/{authorid}", async Task<Results<Ok, NotFound>> (int authorid, PubContext db) =>
-        {
-            var affected = await db.Authors
-                .Where(model => model.AuthorId == authorid)
-                .ExecuteDeleteAsync();
-            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
-        })
-        .WithName("DeleteAuthor")
-        .WithOpenApi();
+        group.MapGet("/WithBooks/", GetAllWithBooks);
+        group.MapGet("/{authorId}", GetById);
+        group.MapPut("/{authorId}", Update);
+        group.MapPost("/", Create);
+        group.MapDelete("/{authorId}", Delete);
     }
-}
+    private static async Task<List<AuthorWithBookResponse>> GetAllWithBooks(PubContext db)
+    {
+        return await db.Authors.Include(a => a.Books).AsNoTracking()
+            .Select(x => new AuthorWithBookResponse
+            {
+                AuthorId = x.AuthorId,
+                FirstName = x.Name.FirstName,
+                LastName = x.Name.LastName,
+                Books = x.Books.Select(x => new BookResponse(x.BookId, x.Title, x.PublishDate)).ToList()
+            }).ToListAsync();
+    }
+    private static async Task<Results<Ok<AuthorResponse>, NotFound>> GetById(int authorId, PubContext db)
+    {
+        return await db.Authors.AsNoTracking()
+            .Where(model => model.AuthorId == authorId)
+            .Select(a => new AuthorResponse(a.AuthorId, a.Name.FirstName, a.Name.LastName))
+            .FirstOrDefaultAsync() is { } model
+            ? TypedResults.Ok(model)
+            : TypedResults.NotFound();
+    }
+    private static async Task<Results<Ok, NotFound>> Update(int authorId, AuthorRequest request, PubContext db)
+    {
+        var affected = await db.Authors.Where(model => model.AuthorId == authorId)
+            .ExecuteUpdateAsync(setters => setters
+                .SetProperty(m => m.Name.FirstName, request.FirstName)
+                .SetProperty(m => m.Name.LastName, request.LastName));
+        return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+    }
+    private static async Task<Created<AuthorResponse>> Create(AuthorRequest request, PubContext db)
+    {
+        var author = new Author { Name = new PersonName { FirstName = request.FirstName, LastName = request.LastName } };
+        db.Authors.Add(author);
+        await db.SaveChangesAsync();
+        return TypedResults.Created($"/api/Author/{author.AuthorId}", new AuthorResponse(author.AuthorId, author.Name.FirstName, author.Name.LastName));
+    }
+    private static async Task<Results<Ok, NotFound>> Delete(int authorId, PubContext db)
+    {
+        var affected = await db.Authors.Where(model => model.AuthorId == authorId)
+            .ExecuteDeleteAsync();
+        return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+    }
+    private record AuthorRequest(string FirstName, string LastName);
+    private record AuthorResponse(int AuthorId, string FirstName, string LastName);
 
+    private class AuthorWithBookResponse
+    {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<BookResponse> Books { get; set; }
+    }
+
+    private record BookResponse(int BookId, string Title, DateOnly PublishDate);
+}
